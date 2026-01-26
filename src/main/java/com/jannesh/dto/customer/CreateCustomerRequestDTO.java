54b77e09536d441008478c767dd4a65f328b2848
Jannesh Rao <janneshr007@gmail.com>
2026-01-26 12:20:26 +0530

    bugfix: refactored customer with dto

---
+++ src/main/java/com/jannesh/dto/customer/CreateCustomerRequestDTO.java
@@ -0,0 +1,21 @@
+package com.jannesh.dto.customer;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class CreateCustomerRequestDTO {
+    @NotBlank(message = "Customer name is required")
+    private String name;
+
+    @NotBlank(message = "Contact number is required")
+    @Size(min = 10, max = 15, message = "Contact number must be between 10 and 15 digits")
+    private String contact;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+}
